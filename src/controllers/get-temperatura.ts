220c5d435dd57d7b43ff6bb2aff86d1dc7b19437
Darlan Jr <kelgaxd@gmail.com>
2022-04-05 04:08:20 -0300

    update getTemperaturaLocal

--- src/controllers/get-temperatura.ts
+++ src/controllers/get-temperatura.ts
@@ -1,18 +1,22 @@
-import axios from "axios";
+const axios = require ("axios");
 
 //Formando o url certo
 const urlKey = 'https://api.hgbrasil.com/weather?key=84db58cc';
 const separador = '&';
-const cidade = 'CampoGrande,MS';
+const cidade = 'city_name=' + 'Campo Grande,MS';
 
 //concatenando o url para o get
 const url = urlKey + separador + cidade;
 
-function getTemperaturaLocal() {
-    axios.get(url)
+async function getTemperaturaLocal() {
+    await axios.get(url)
     .then(response => {
-        console.log(response)
-    })
+        console.log('Local: ' + response.data.results.city);
+        console.log('Data: ' + response.data.results.date);
+        console.log('Hora: ' + response.data.results.time);
+        console.log('Temperatura: ' + response.data.results.temp + '°');
+    })  
     .catch(error => console.log(error))
 }
 
+getTemperaturaLocal()
